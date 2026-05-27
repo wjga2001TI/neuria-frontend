a6be9cb55bb0a90845907a94e246bbda2d82a9b6
will-tzm <wjga2001.unitec@gmail.com>
2026-05-27 14:46:21 -0400

    Fix phone-number-validator

--- src/app/module/account/profile/profile.component.ts
+++ src/app/module/account/profile/profile.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 import { AuthenticationService } from 'app/core/service/authentication.service';
 import { PaymentService } from 'app/core/service/payment.service';
 // @ts-ignore
-import intlTelInput from 'phone-number-validator/build/js/intlTelInput.min.js';
+import intlTelInput from 'phone-number-validator/js/intlTelInput.min.js';
 import { parsePhoneNumberFromString } from 'libphonenumber-js';
 
 @Component({
--- src/app/module/auth/register/register.component.ts
+++ src/app/module/auth/register/register.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthenticationService } from 'app/core/service/authentication.service';
 // @ts-ignore
-import intlTelInput from 'phone-number-validator/build/js/intlTelInput.min.js';
+import intlTelInput from 'phone-number-validator/js/intlTelInput.min.js';
 import { parsePhoneNumberFromString } from 'libphonenumber-js';
 
 @Component({
